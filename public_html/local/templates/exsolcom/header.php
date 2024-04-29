478d100ae0cf09c65a6835d3de80a129fc5837bc
poluektovaalina <poluektova2315@gmail.com>
2024-04-29 16:40:43 +0500

    -Подключили шрифт. Создала два контейнера HEADER LEFT/RIGHT, внутри элементам и блокам задала стили. HEADER MENU и HEADER MORE задала hover при наведении. в HTML и SCSS оставила комментарии.

--- public_html/local/templates/exsolcom/header.php
+++ public_html/local/templates/exsolcom/header.php
@@ -10,8 +10,44 @@ if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true) die();
     ?>
 </head>
 <header class="i_header">
-	<div class="i_header-burger"></div>
-	<?$APPLICATION->IncludeFile(SITE_TEMPLATE_PATH.'/ilab/inc/header_menu.php',Array(),Array('MODE'=>'html', 'NAME'=>'Вверхнее меню', 'SHOW_BORDER'=>false))// Menu?>
-	<div class="i_header-blog"></div>
-	<div class="i_header-more"></div>
+    <div class="i_container">
+        <!--HEADER LEFT-->
+        <div class="i_header-left">
+            <!--HEADER BURGER-->
+            <div class="i_header-burger">
+                <img src="<?=SITE_TEMPLATE_PATH.'/ilab/img/svg/burger-icon.svg'?>" alt="burger-icon">
+            </div>
+            <!--!HEADER BURGER-->
+
+            <!--HEADER MENU-->
+            <?$APPLICATION->IncludeFile(SITE_TEMPLATE_PATH.'/ilab/inc/header_menu.php',Array(),Array('MODE'=>'html', 'NAME'=>'Вверхнее меню', 'SHOW_BORDER'=>false))// Menu?>
+            <!--!HEADER MENU-->
+        </div>
+        <!--!HEADER LEFT-->
+
+        <!--HEADER RIGHT-->
+        <div class="i_header-right">
+            <!--HEADER BLOG-->
+            <button class="i_header-blog">Блог</button>
+            <!--!HEADER BLOG-->
+
+            <!--HEADER MORE-->
+            <div class="i_header-more">
+                <div class="i_header-circles">
+                    <span>RU</span>
+                </div>
+                <div class="i_header-circles">
+                    <img src="<?=SITE_TEMPLATE_PATH.'/ilab/img/svg/lupa-icon.svg'?>" alt="burger-icon">
+                </div>
+                <div class="i_header-circles">
+                    <img c src="<?=SITE_TEMPLATE_PATH.'/ilab/img/svg/whatsapp-icon.svg'?>" alt="burger-icon">
+                </div>
+                <div class="i_header-circles">
+                    <img src="<?=SITE_TEMPLATE_PATH.'/ilab/img/svg/email-icon.svg'?>" alt="burger-icon">
+                </div>
+            </div>
+            <!--!HEADER MORE-->
+        </div>
+        <!--!HEADER RIGHT-->
+    </div>
 </header>
