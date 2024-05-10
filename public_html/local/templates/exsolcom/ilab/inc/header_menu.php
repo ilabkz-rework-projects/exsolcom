a5f6074351ed930c4fd506f45420aea00c09ce63
Руслан <rd@ilab.kz>
2024-05-10 16:14:27 +0500

    Структурировал файлы

--- public_html/local/templates/exsolcom/ilab/inc/header_menu.php
+++ public_html/local/templates/exsolcom/ilab/inc/header_menu.php
@@ -1,17 +1,19 @@
 <?$APPLICATION->IncludeComponent(
-	"bitrix:menu",
-	"ilab.h_nav",
-	Array(
+	"bitrix:menu", 
+	"ilab.h_nav", 
+	array(
 		"ALLOW_MULTI_SELECT" => "N",
 		"CHILD_MENU_TYPE" => "left",
-		"COMPONENT_TEMPLATE" => "vertical_multilevel",
+		"COMPONENT_TEMPLATE" => "ilab.h_nav",
 		"DELAY" => "N",
 		"MAX_LEVEL" => "1",
-		"MENU_CACHE_GET_VARS" => "",
-		"MENU_CACHE_TIME" => "3600",
-		"MENU_CACHE_TYPE" => "N",
+		"MENU_CACHE_GET_VARS" => array(
+		),
+		"MENU_CACHE_TIME" => "360000",
+		"MENU_CACHE_TYPE" => "A",
 		"MENU_CACHE_USE_GROUPS" => "Y",
 		"ROOT_MENU_TYPE" => "top",
 		"USE_EXT" => "N"
-	)
+	),
+	false
 );?>
