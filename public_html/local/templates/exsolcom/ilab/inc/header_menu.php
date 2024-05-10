f660f65a5c04a053df7fabe5d8bad8c7d60640df
Руслан <rd@ilab.kz>
2024-05-10 17:43:20 +0500

    Добавление разделов новостей и блоГ

--- public_html/local/templates/exsolcom/ilab/inc/header_menu.php
+++ public_html/local/templates/exsolcom/ilab/inc/header_menu.php
@@ -1,19 +1,17 @@
 <?$APPLICATION->IncludeComponent(
-	"bitrix:menu", 
-	"ilab.h_nav", 
-	array(
+	"bitrix:menu",
+	"ilab.h_nav",
+	Array(
 		"ALLOW_MULTI_SELECT" => "N",
 		"CHILD_MENU_TYPE" => "left",
-		"COMPONENT_TEMPLATE" => "ilab.h_nav",
+		"COMPONENT_TEMPLATE" => "vertical_multilevel",
 		"DELAY" => "N",
 		"MAX_LEVEL" => "1",
-		"MENU_CACHE_GET_VARS" => array(
-		),
-		"MENU_CACHE_TIME" => "360000",
-		"MENU_CACHE_TYPE" => "A",
+		"MENU_CACHE_GET_VARS" => "",
+		"MENU_CACHE_TIME" => "3600",
+		"MENU_CACHE_TYPE" => "N",
 		"MENU_CACHE_USE_GROUPS" => "Y",
 		"ROOT_MENU_TYPE" => "top",
 		"USE_EXT" => "N"
-	),
-	false
+	)
 );?>
