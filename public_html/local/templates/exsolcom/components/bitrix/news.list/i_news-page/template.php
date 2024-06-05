026d49f115c78be62883d394c3b06c1cf3b42683
poluektovaalina <poluektova2315@gmail.com>
2024-06-05 18:43:15 +0500

    - застилизовала страницу новости.

--- public_html/local/templates/exsolcom/components/bitrix/news.list/i_news-page/template.php
+++ public_html/local/templates/exsolcom/components/bitrix/news.list/i_news-page/template.php
@@ -12,7 +12,8 @@
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
 ?>
-<div class="news-list">
+
+<div class="news-page-list">
 	<?if($arParams["DISPLAY_TOP_PAGER"]):?>
 		<?=$arResult["NAV_STRING"]?>
 	<?endif;?>
@@ -21,7 +22,7 @@ $this->setFrameMode(true);
 		$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
 		$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
 		?>
-		<div class="news-item" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
+		<div class="news-page-item" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
 			<?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
 				<div class="news-item-img">
 					<?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
