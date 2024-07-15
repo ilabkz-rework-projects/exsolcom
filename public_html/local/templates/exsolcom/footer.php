a8398658180c65ecde536cc38e827bbfe61f6629
poluektovaalina <poluektova2315@gmail.com>
2024-07-15 15:47:52 +0500

    refactor: темная тема и адаптив модального окна - "Проекты" готов;

--- public_html/local/templates/exsolcom/footer.php
+++ public_html/local/templates/exsolcom/footer.php
@@ -311,9 +311,11 @@
 		<div class="i_modal-close">
 			<img src="<?=SITE_TEMPLATE_PATH.'/ilab/img/svg/closeModal-dark.svg'?>" alt="x">
 		</div>
-		<div class="i_modal-img"></div>
+        <div class="i_modal-box">
+            <div class="i_modal-img"></div>
 
-		<div class="i_modal-content"></div>
+            <div class="i_modal-content"></div>
+        </div>
 
 		<div class="i_modal-footer">
 			<div class="i_modal-footer-price idn"></div>
