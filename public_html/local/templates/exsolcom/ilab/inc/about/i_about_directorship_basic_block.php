474f71de2e8c68079f28020fc37e4c7b8e2c064f
poluektovaalina <poluektova2315@gmail.com>
2024-07-24 14:46:34 +0500

    refactor: разбила компоненты на сниппеты на странице: О компании.

---
+++ public_html/local/templates/exsolcom/ilab/inc/about/i_about_directorship_basic_block.php
@@ -0,0 +1,44 @@
+<div class="i_directorship-basic-block">
+
+    <div class="i_directorship-basic-block-wrapper">
+        <div class="i_directorship-basic-block-portrait">
+            <img src="<?= SITE_TEMPLATE_PATH . '/ilab/img/main/picture-8-Director.png' ?>"
+                 alt="picture-8-Director">
+        </div>
+    </div>
+
+
+    <div class="i_directorship-basic-block-desc">
+
+        <div class="i_directorship-basic-block-desc-top">
+                        <div class="i_directorship-block-desc-top-name">
+								<span class="i_directorship-desc-top-name-name">Мастяев <span
+                                        class="color-red-top-name">Владимир Валерьевич</span></span>
+                <span class="i_directorship-desc-top-name-specification">
+                                Партнер компании «Exsolcom»
+                            </span>
+            </div>
+
+
+            <div class="i_directorship-block-desc-top-depiction">
+                <span class="i_directorship-desc-top-depiction-name">Директор</span>
+                <div class="i_directorship-desc-top-depiction-specification">
+                    <span>Высшее экономическое образование.</span>
+                    <span>Опыт работы в качестве консультанта и руководителя проектов более 15 лет</span>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="i_directorship-basic-block-desc-bottom">
+            <div class="i_directorship-basic-block-desc-bottom-container">
+                            <span>
+                                «Миссией нашей компании является необходимость дать возможность нашим клиентам достигнуть максимального успеха в бизнесе посредством использования наших оптимальных экспертных ІТ-решений, продуктов и услуг. У нас амбициозные стратегические планы на будущее, мы растем из года в год, предоставляя качественные решения нашим клиентам!»
+                            </span>
+            </div>
+        </div>
+
+
+    </div>
+
+</div>
---
+++ public_html/local/templates/exsolcom/ilab/inc/about/i_about_directorship_basic_block_second.php
@@ -0,0 +1,45 @@
+<div class="i_directorship-basic-block_second">
+
+    <div class="i_directorship-basic-block-wrapper_second">
+        <div class="i_directorship-basic-block-portrait">
+            <img src="<?= SITE_TEMPLATE_PATH . '/ilab/img/main/picture-9-Technical-Director.png' ?>"
+                 alt="picture-9-Technical-Director">
+        </div>
+    </div>
+
+
+    <div class="i_directorship-basic-block-desc_second">
+
+        <div class="i_directorship-basic-block-desc-top_second">
+
+            <div class="i_directorship-block-desc-top-name">
+								<span class="i_directorship-desc-top-name-name">Прокофьев <span
+                                        class="color-red-top-name">Алексей Вячеславович</span></span>
+                <span class="i_directorship-desc-top-name-specification">
+                                Партнер компании «Exsolcom»
+                            </span>
+            </div>
+
+            <div class="i_directorship-block-desc-top-depiction">
+                <span class="i_directorship-desc-top-depiction-name">Технический директор</span>
+                <div class="i_directorship-desc-top-depiction-specification">
+                    <span>Высшее экономическое и техническое образование</span>
+                    <span>Опыт работы в качестве программиста-разработчика более 20 лет</span>
+                    <span>Опыт работы в качестве 1С программиста более 17 лет</span>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="i_directorship-basic-block-desc-bottom_second">
+            <div class="i_directorship-basic-block-desc-bottom-container">
+                            <span>
+                                «Мы стремимся в своей работе к высокому качеству продуктов и оказываемых услуг, постоянно совершенствуя технологию работы, качество работы с клиентами и профессионализм сотрудников»
+                            </span>
+            </div>
+        </div>
+
+
+    </div>
+
+</div>
