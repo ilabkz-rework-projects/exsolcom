5166f4acb88407fbf3f15ddf7918c01ffc5b4c6f
Руслан <rd@ilab.kz>
2024-07-24 15:33:00 +0500

    разбил компоненту на сниппет в о компании

---
+++ public_html/local/templates/exsolcom/components/bitrix/news.list/i_about_stuff/template.php
@@ -0,0 +1,67 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+?>
+<div class="i_team-personal">
+<?if($arParams["DISPLAY_TOP_PAGER"]):?>
+	<?=$arResult["NAV_STRING"]?><br />
+<?endif;?>
+<?foreach($arResult["ITEMS"] as $arItem):?>
+	<?
+	$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+	$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+	?>
+	<div class="i_team-personal-block" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
+		<!--TEAM PERSONAL BLOCK DEPICTION-->
+		<div class="i_team-personal-block-depiction">
+			<!--TEAM PERSONAL BLOCK PICTURE-->
+			<div class="i_team-personal-block-picture">
+				<div class="i_team-personal-block-picture-block">
+					<img src="<?= $arItem['PREVIEW_PICTURE']['SRC'] ?>"
+					     alt="picture-1-Head">
+				</div>
+			</div>
+			<!--!TEAM PERSONAL BLOCK PICTURE-->
+			<!--TEAM PERSONAL BLOCK DEPICTION GROUP-->
+			<div class="i_team-personal-block-depiction-group">
+				<!--TEAM PERSONAL BLOCK DEPICTION GROUP NAME-->
+				<div class="block-depiction-group-name">
+					<span><?= $arItem['NAME'] ?></span>
+				</div>
+				<!--!TEAM PERSONAL BLOCK DEPICTION GROUP NAME-->
+				<!--TEAM PERSONAL BLOCK DEPICTION GROUP TITLE-->
+				<div class="block-depiction-group-title">
+					<p><?= $arItem['PROPERTIES']['I_ROLE']['VALUE'] ?></p>
+				</div>
+				<!--!TEAM PERSONAL BLOCK DEPICTION GROUP TITLE-->
+				<!--TEAM PERSONAL BLOCK DEPICTION GROUP DESC-->
+				<div class="block-depiction-group-desc">
+					<p><?=$arItem['PREVIEW_TEXT']?></p>
+				</div>
+				<!--!TEAM PERSONAL BLOCK DEPICTION GROUP DESC-->
+			</div>
+			<!--!TEAM PERSONAL BLOCK DEPICTION GROUP-->
+		</div>
+		<!--!TEAM PERSONAL BLOCK DEPICTION-->
+	</div>
+<?endforeach;?>
+<?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
+	<br /><?=$arResult["NAV_STRING"]?>
+<?endif;?>
+</div>
+
+<?php /*
+echo '<pre class="pre">';
+print_r($arResult["ITEMS"]);
+echo '</pre>';
+*/?>
--- public_html/local/templates/exsolcom/ilab/inc/i_about-us.php
+++ public_html/local/templates/exsolcom/ilab/inc/i_about-us.php
@@ -114,254 +114,67 @@
 				<!--!TEAM TITLE NAME-->
 			</div>
 			<!--!TEAM TITLE-->
-			<!--TEAM PERSONALS-->
-			<div class="i_team-personal">
-				<!--TEAM PERSONAL BLOCK-->
-				<div class="i_team-personal-block">
 
-					<!--TEAM PERSONAL BLOCK DEPICTION-->
-					<div class="i_team-personal-block-depiction">
-						<!--TEAM PERSONAL BLOCK PICTURE-->
-						<div class="i_team-personal-block-picture">
-							<div class="i_team-personal-block-picture-block">
-								<img src="<?= SITE_TEMPLATE_PATH . '/ilab/img/main/picture-1-Head.png' ?>"
-								     alt="picture-1-Head">
-							</div>
-						</div>
-						<!--!TEAM PERSONAL BLOCK PICTURE-->
-						<!--TEAM PERSONAL BLOCK DEPICTION GROUP-->
-						<div class="i_team-personal-block-depiction-group">
-							<!--TEAM PERSONAL BLOCK DEPICTION GROUP NAME-->
-							<div class="block-depiction-group-name">
-								<span>Юлия<br>Лозовецкая</span>
-							</div>
-							<!--!TEAM PERSONAL BLOCK DEPICTION GROUP NAME-->
-							<!--TEAM PERSONAL BLOCK DEPICTION GROUP TITLE-->
-							<div class="block-depiction-group-title">
-								<p>Руководитель проектов</p>
-							</div>
-							<!--!TEAM PERSONAL BLOCK DEPICTION GROUP TITLE-->
-							<!--TEAM PERSONAL BLOCK DEPICTION GROUP DESC-->
-							<div class="block-depiction-group-desc">
-								<p>Менеджер в турагентстве продает туры конкретным клиентам, обрабатывая заявки,
-									подбирая подходящие варианты, оформляя документы, визы....</p>
-							</div>
-							<!--!TEAM PERSONAL BLOCK DEPICTION GROUP DESC-->
-						</div>
-						<!--!TEAM PERSONAL BLOCK DEPICTION GROUP-->
-					</div>
-					<!--!TEAM PERSONAL BLOCK DEPICTION-->
-				</div>
-				<div class="i_team-personal-block">
+			<!-- TEAM STUFF	-->
+			<?$APPLICATION->IncludeComponent(
+				"bitrix:news.list",
+				"i_about_stuff",
+				Array(
+					"ACTIVE_DATE_FORMAT" => "d.m.Y",
+					"ADD_SECTIONS_CHAIN" => "Y",
+					"AJAX_MODE" => "N",
+					"AJAX_OPTION_ADDITIONAL" => "",
+					"AJAX_OPTION_HISTORY" => "N",
+					"AJAX_OPTION_JUMP" => "N",
+					"AJAX_OPTION_STYLE" => "Y",
+					"CACHE_FILTER" => "N",
+					"CACHE_GROUPS" => "Y",
+					"CACHE_TIME" => "36000000",
+					"CACHE_TYPE" => "A",
+					"CHECK_DATES" => "Y",
+					"DETAIL_URL" => "",
+					"DISPLAY_BOTTOM_PAGER" => "Y",
+					"DISPLAY_DATE" => "Y",
+					"DISPLAY_NAME" => "Y",
+					"DISPLAY_PICTURE" => "Y",
+					"DISPLAY_PREVIEW_TEXT" => "Y",
+					"DISPLAY_TOP_PAGER" => "N",
+					"FIELD_CODE" => array(0=>"",1=>"",),
+					"FILTER_NAME" => "",
+					"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+					"IBLOCK_ID" => "6",
+					"IBLOCK_TYPE" => "Content",
+					"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+					"INCLUDE_SUBSECTIONS" => "Y",
+					"MESSAGE_404" => "",
+					"NEWS_COUNT" => "20",
+					"PAGER_BASE_LINK_ENABLE" => "N",
+					"PAGER_DESC_NUMBERING" => "N",
+					"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+					"PAGER_SHOW_ALL" => "N",
+					"PAGER_SHOW_ALWAYS" => "N",
+					"PAGER_TEMPLATE" => ".default",
+					"PAGER_TITLE" => "Новости",
+					"PARENT_SECTION" => "",
+					"PARENT_SECTION_CODE" => "",
+					"PREVIEW_TRUNCATE_LEN" => "",
+					"PROPERTY_CODE" => array(0=>"",1=>"",),
+					"SET_BROWSER_TITLE" => "Y",
+					"SET_LAST_MODIFIED" => "N",
+					"SET_META_DESCRIPTION" => "Y",
+					"SET_META_KEYWORDS" => "Y",
+					"SET_STATUS_404" => "N",
+					"SET_TITLE" => "Y",
+					"SHOW_404" => "N",
+					"SORT_BY1" => "ACTIVE_FROM",
+					"SORT_BY2" => "SORT",
+					"SORT_ORDER1" => "DESC",
+					"SORT_ORDER2" => "ASC",
+					"STRICT_SECTION_CHECK" => "N"
+				)
+			);?>
+			<!-- !TEAM STUFF	-->
 
-					<!--TEAM PERSONAL BLOCK DEPICTION-->
-					<div class="i_team-personal-block-depiction">
-						<!--TEAM PERSONAL BLOCK PICTURE-->
-						<div class="i_team-personal-block-picture">
-							<div class="i_team-personal-block-picture-block">
-								<img src="<?= SITE_TEMPLATE_PATH . '/ilab/img/main/picture-2-Head.png' ?>"
-								     alt="picture-2-Head">
-							</div>
-						</div>
-						<!--!TEAM PERSONAL BLOCK PICTURE-->
-						<!--TEAM PERSONAL BLOCK DEPICTION GROUP-->
-						<div class="i_team-personal-block-depiction-group">
-							<!--TEAM PERSONAL BLOCK DEPICTION GROUP NAME-->
-							<div class="block-depiction-group-name">
-								<span>Олег<br>Тихоновский</span>
-							</div>
-							<!--!TEAM PERSONAL BLOCK DEPICTION GROUP NAME-->
-							<!--TEAM PERSONAL BLOCK DEPICTION GROUP TITLE-->
-							<div class="block-depiction-group-title">
-								<p>Руководитель проектов</p>
-							</div>
-							<!--!TEAM PERSONAL BLOCK DEPICTION GROUP TITLE-->
-							<!--TEAM PERSONAL BLOCK DEPICTION GROUP DESC-->
-							<div class="block-depiction-group-desc">
-								<p>Менеджер в турагентстве продает туры конкретным клиентам, обрабатывая заявки,
-									подбирая подходящие варианты, оформляя документы, визы....</p>
-							</div>
-							<!--!TEAM PERSONAL BLOCK DEPICTION GROUP DESC-->
-						</div>
-						<!--!TEAM PERSONAL BLOCK DEPICTION GROUP-->
-					</div>
-					<!--!TEAM PERSONAL BLOCK DEPICTION-->
-				</div>
-				<div class="i_team-personal-block">
-
-					<!--TEAM PERSONAL BLOCK DEPICTION-->
-					<div class="i_team-personal-block-depiction">
-						<!--TEAM PERSONAL BLOCK PICTURE-->
-						<div class="i_team-personal-block-picture">
-							<div class="i_team-personal-block-picture-block">
-								<img src="<?= SITE_TEMPLATE_PATH . '/ilab/img/main/picture-3-Head.png' ?>"
-								     alt="picture-3-Head">
-							</div>
-						</div>
-						<!--!TEAM PERSONAL BLOCK PICTURE-->
-						<!--TEAM PERSONAL BLOCK DEPICTION GROUP-->
-						<div class="i_team-personal-block-depiction-group">
-							<!--TEAM PERSONAL BLOCK DEPICTION GROUP NAME-->
-							<div class="block-depiction-group-name">
-								<span>Владимир<br>Яровой</span>
-							</div>
-							<!--!TEAM PERSONAL BLOCK DEPICTION GROUP NAME-->
-							<!--TEAM PERSONAL BLOCK DEPICTION GROUP TITLE-->
-							<div class="block-depiction-group-title">
-								<p>Руководитель отдела продаж</p>
-							</div>
-							<!--!TEAM PERSONAL BLOCK DEPICTION GROUP TITLE-->
-							<!--TEAM PERSONAL BLOCK DEPICTION GROUP DESC-->
-							<div class="block-depiction-group-desc">
-								<p>Менеджер в турагентстве продает туры конкретным клиентам, обрабатывая заявки,
-									подбирая подходящие варианты, оформляя документы, визы....</p>
-							</div>
-							<!--!TEAM PERSONAL BLOCK DEPICTION GROUP DESC-->
-						</div>
-						<!--!TEAM PERSONAL BLOCK DEPICTION GROUP-->
-					</div>
-					<!--!TEAM PERSONAL BLOCK DEPICTION-->
-				</div>
-				<div class="i_team-personal-block">
-
-					<!--TEAM PERSONAL BLOCK DEPICTION-->
-					<div class="i_team-personal-block-depiction">
-						<!--TEAM PERSONAL BLOCK PICTURE-->
-						<div class="i_team-personal-block-picture">
-							<div class="i_team-personal-block-picture-block">
-								<img src="<?= SITE_TEMPLATE_PATH . '/ilab/img/main/picture-4-Head.png' ?>"
-								     alt="picture-4-Head">
-							</div>
-						</div>
-						<!--!TEAM PERSONAL BLOCK PICTURE-->
-						<!--TEAM PERSONAL BLOCK DEPICTION GROUP-->
-						<div class="i_team-personal-block-depiction-group">
-							<!--TEAM PERSONAL BLOCK DEPICTION GROUP NAME-->
-							<div class="block-depiction-group-name">
-								<span>Мария<br>Ломановская</span>
-							</div>
-							<!--!TEAM PERSONAL BLOCK DEPICTION GROUP NAME-->
-							<!--TEAM PERSONAL BLOCK DEPICTION GROUP TITLE-->
-							<div class="block-depiction-group-title">
-								<p>Руководитель проектов</p>
-							</div>
-							<!--!TEAM PERSONAL BLOCK DEPICTION GROUP TITLE-->
-							<!--TEAM PERSONAL BLOCK DEPICTION GROUP DESC-->
-							<div class="block-depiction-group-desc">
-								<p>Менеджер в турагентстве продает туры конкретным клиентам, обрабатывая заявки,
-									подбирая подходящие варианты, оформляя документы, визы....</p>
-							</div>
-							<!--!TEAM PERSONAL BLOCK DEPICTION GROUP DESC-->
-						</div>
-						<!--!TEAM PERSONAL BLOCK DEPICTION GROUP-->
-					</div>
-					<!--!TEAM PERSONAL BLOCK DEPICTION-->
-				</div>
-				<div class="i_team-personal-block">
-					<!--TEAM PERSONAL BLOCK DEPICTION-->
-					<div class="i_team-personal-block-depiction">
-						<!--TEAM PERSONAL BLOCK PICTURE-->
-						<div class="i_team-personal-block-picture">
-							<div class="i_team-personal-block-picture-block">
-								<img src="<?= SITE_TEMPLATE_PATH . '/ilab/img/main/picture-5-Manager.png' ?>"
-								     alt="picture-5-Manager">
-							</div>
-						</div>
-						<!--!TEAM PERSONAL BLOCK PICTURE-->
-						<!--TEAM PERSONAL BLOCK DEPICTION GROUP-->
-						<div class="i_team-personal-block-depiction-group">
-							<!--TEAM PERSONAL BLOCK DEPICTION GROUP NAME-->
-							<div class="block-depiction-group-name">
-								<span>Аида<br>Баимбетова</span>
-							</div>
-							<!--!TEAM PERSONAL BLOCK DEPICTION GROUP NAME-->
-							<!--TEAM PERSONAL BLOCK DEPICTION GROUP TITLE-->
-							<div class="block-depiction-group-title">
-								<p>Менеджер по продажам 1СFresh</p>
-							</div>
-							<!--!TEAM PERSONAL BLOCK DEPICTION GROUP TITLE-->
-							<!--TEAM PERSONAL BLOCK DEPICTION GROUP DESC-->
-							<div class="block-depiction-group-desc">
-								<p>Менеджер в турагентстве продает туры конкретным клиентам, обрабатывая заявки,
-									подбирая подходящие варианты, оформляя документы, визы....</p>
-							</div>
-							<!--!TEAM PERSONAL BLOCK DEPICTION GROUP DESC-->
-						</div>
-						<!--!TEAM PERSONAL BLOCK DEPICTION GROUP-->
-					</div>
-					<!--!TEAM PERSONAL BLOCK DEPICTION-->
-				</div>
-				<div class="i_team-personal-block">
-					<!--TEAM PERSONAL BLOCK DEPICTION-->
-					<div class="i_team-personal-block-depiction">
-						<!--TEAM PERSONAL BLOCK PICTURE-->
-						<div class="i_team-personal-block-picture">
-							<div class="i_team-personal-block-picture-block">
-								<img src="<?= SITE_TEMPLATE_PATH . '/ilab/img/main/picture-6-Manager.png' ?>"
-								     alt="picture-6-Manager">
-							</div>
-						</div>
-						<!--!TEAM PERSONAL BLOCK PICTURE-->
-						<!--TEAM PERSONAL BLOCK DEPICTION GROUP-->
-						<div class="i_team-personal-block-depiction-group">
-							<!--TEAM PERSONAL BLOCK DEPICTION GROUP NAME-->
-							<div class="block-depiction-group-name">
-								<span>Данияр<br>Утепов</span>
-							</div>
-							<!--!TEAM PERSONAL BLOCK DEPICTION GROUP NAME-->
-							<!--TEAM PERSONAL BLOCK DEPICTION GROUP TITLE-->
-							<div class="block-depiction-group-title">
-								<p>Менеджер по продажам 1СFresh</p>
-							</div>
-							<!--!TEAM PERSONAL BLOCK DEPICTION GROUP TITLE-->
-							<!--TEAM PERSONAL BLOCK DEPICTION GROUP DESC-->
-							<div class="block-depiction-group-desc">
-								<p>Менеджер в турагентстве продает туры конкретным клиентам, обрабатывая заявки,
-									подбирая подходящие варианты, оформляя документы, визы....</p>
-							</div>
-							<!--!TEAM PERSONAL BLOCK DEPICTION GROUP DESC-->
-						</div>
-						<!--!TEAM PERSONAL BLOCK DEPICTION GROUP-->
-					</div>
-					<!--!TEAM PERSONAL BLOCK DEPICTION-->
-				</div>
-				<div class="i_team-personal-block">
-					<!--TEAM PERSONAL BLOCK DEPICTION-->
-					<div class="i_team-personal-block-depiction">
-						<!--TEAM PERSONAL BLOCK PICTURE-->
-						<div class="i_team-personal-block-picture">
-							<div class="i_team-personal-block-picture-block">
-								<img src="<?= SITE_TEMPLATE_PATH . '/ilab/img/main/picture-7-Manager.png' ?>"
-								     alt="picture-7-Manager">
-							</div>
-						</div>
-						<!--!TEAM PERSONAL BLOCK PICTURE-->
-						<!--TEAM PERSONAL BLOCK DEPICTION GROUP-->
-						<div class="i_team-personal-block-depiction-group">
-							<!--TEAM PERSONAL BLOCK DEPICTION GROUP NAME-->
-							<div class="block-depiction-group-name">
-								<span>Артур<br>Якупов</span>
-							</div>
-							<!--!TEAM PERSONAL BLOCK DEPICTION GROUP NAME-->
-							<!--TEAM PERSONAL BLOCK DEPICTION GROUP TITLE-->
-							<div class="block-depiction-group-title">
-								<p>Менеджер по продажам</p>
-							</div>
-							<!--!TEAM PERSONAL BLOCK DEPICTION GROUP TITLE-->
-							<!--TEAM PERSONAL BLOCK DEPICTION GROUP DESC-->
-							<div class="block-depiction-group-desc">
-								<p>Менеджер в турагентстве продает туры конкретным клиентам, обрабатывая заявки,
-									подбирая подходящие варианты, оформляя документы, визы....</p>
-							</div>
-							<!--!TEAM PERSONAL BLOCK DEPICTION GROUP DESC-->
-						</div>
-						<!--!TEAM PERSONAL BLOCK DEPICTION GROUP-->
-					</div>
-					<!--!TEAM PERSONAL BLOCK DEPICTION-->
-				</div>
-				<!--!TEAM PERSONAL BLOCK-->
-			</div>
-			<!--!TEAM PERSONALS-->
 		</div>
 	</div>
 	<!--!TEAM-->
