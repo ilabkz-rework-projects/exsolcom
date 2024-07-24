c09bc5bcf771e485a9e0f22bdad66d3c505512d2
poluektovaalina <poluektova2315@gmail.com>
2024-07-24 18:05:10 +0500

    refactor: на странице: О компании: разбила компоненту на сниппет в блоке "Наши сертификаты" .

---
+++ public_html/local/templates/exsolcom/ilab/inc/about/work_process_blocks/i_work_process_block_1.php
@@ -0,0 +1,9 @@
+<div class="i_work-process-body-group-block">
+
+    <span class="i_work-process-body-group-block-color">Анализ, Отчет Обследования</span>
+
+    <span>
+                            Анализ существующих процессов, разработка рекомендаций по оптимизации и подготовка к информационной системе
+                        </span>
+
+</div>
---
+++ public_html/local/templates/exsolcom/ilab/inc/about/work_process_blocks/i_work_process_block_5.php
@@ -0,0 +1,9 @@
+<div class="i_work-process-body-group-block">
+
+    <span class="i_work-process-body-group-block-color">Обучение сотрудников</span>
+
+    <span>
+                            Подготовка сотрудников к использованию нового инструмента: обучение основным принципам работы, подготовка инструкций по ролям
+                        </span>
+
+</div>
