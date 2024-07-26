1307dd085cb4f5e138c97aee7d16a7d1934ca4f0
poluektovaalina <poluektova2315@gmail.com>
2024-07-26 11:28:56 +0500

    refactor: настроила кнопку : на странице о компании для скачивания.

--- public_html/local/templates/exsolcom/ilab/inc/i_about-us.php
+++ public_html/local/templates/exsolcom/ilab/inc/i_about-us.php
@@ -46,13 +46,7 @@
                 <?$APPLICATION->IncludeFile(SITE_TEMPLATE_PATH.'/ilab/inc/about/i_about_company-strategy.php',Array(),Array('MODE'=>'html', 'NAME'=>'Описание стратегии', 'SHOW_BORDER'=>true))?>
                 <!--!ABOUT COMPANY STRATEGY DESC-->
 				<!--ABOUT COMPANY STRATEGY BTN-->
-				<div class="i_about-company-strategy-btn">
-                    <a href="https://drive.google.com/uc?export=download&id=1wI4qaYHD-5QR3mqtzv7iRFkrj_-40xll" target="_blank" download>
-                        <button>
-                            Скачать презентацию компаний
-                        </button>
-                    </a>
-				</div>
+                <?$APPLICATION->IncludeFile(SITE_TEMPLATE_PATH.'/ilab/inc/about/i_about_company-strategy-btn.php',Array(),Array('MODE'=>'html', 'NAME'=>'Описание стратегии', 'SHOW_BORDER'=>true))?>
 				<!--!ABOUT COMPANY STRATEGY BTN-->
 			</div>
 			<!--!ABOUT COMPANY STRATEGY-->
