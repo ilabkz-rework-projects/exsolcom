6c8aaa65896e2bf8270d38601e8e529c778acdfb
Руслан <rd@ilab.kz>
2024-07-30 18:30:06 +0500

    добаление модального окна вакансии

--- public_html/local/templates/exsolcom/components/bitrix/catalog/i_programm/section_horizontal.php
+++ public_html/local/templates/exsolcom/components/bitrix/catalog/i_programm/section_horizontal.php
@@ -19,6 +19,8 @@ else
 {
 	$basketAction = $arParams['SECTION_ADD_TO_BASKET_ACTION'] ?? '';
 }
+
+
 ?>
 	<div class="<?=($isSidebar ? "col-md-9 col-sm-8" : "col-xs-12")?>">
 		<div class="row">
@@ -182,11 +184,17 @@ else
 			</div>
 			<div class="col-xs-12 programm-container">
 				<?
+				$requestUri = $_SERVER['REQUEST_URI'];
+				// Разбираем URL и извлекаем строку запроса
+				$query = parse_url($requestUri, PHP_URL_QUERY);
+				// Разбираем параметры строки запроса
+				parse_str($query, $params);
+				// Получаем значение параметра SECTION_ID
+				$section_id = $params['SECTION_ID'];
+
 				$sectionListParams = array(
 					"IBLOCK_TYPE" => $arParams["IBLOCK_TYPE"],
 					"IBLOCK_ID" => $arParams["IBLOCK_ID"],
-					"SECTION_ID" => $arResult["VARIABLES"]["SECTION_ID"],
-					"SECTION_CODE" => $arResult["VARIABLES"]["SECTION_CODE"],
 					"CACHE_TYPE" => $arParams["CACHE_TYPE"],
 					"CACHE_TIME" => $arParams["CACHE_TIME"],
 					"CACHE_GROUPS" => $arParams["CACHE_GROUPS"],
@@ -195,8 +203,9 @@ else
 					"SECTION_URL" => $arResult["FOLDER"].$arResult["URL_TEMPLATES"]["section"],
 					"VIEW_MODE" => $arParams["SECTIONS_VIEW_MODE"],
 					"SHOW_PARENT_NAME" => $arParams["SECTIONS_SHOW_PARENT_NAME"],
-					"HIDE_SECTION_NAME" => (isset($arParams["SECTIONS_HIDE_SECTION_NAME"]) ? $arParams["SECTIONS_HIDE_SECTION_NAME"] : "N"),
-					"ADD_SECTIONS_CHAIN" => (isset($arParams["ADD_SECTIONS_CHAIN"]) ? $arParams["ADD_SECTIONS_CHAIN"] : '')
+					"HIDE_SECTION_NAME" => ($arParams["SECTIONS_HIDE_SECTION_NAME"] ?? "N"),
+					"ADD_SECTIONS_CHAIN" => ($arParams["ADD_SECTIONS_CHAIN"] ?? ''),
+					"CURRENT_SECTION_ID" => $section_id
 				);
 				if ($sectionListParams["COUNT_ELEMENTS"] === "Y")
 				{
@@ -206,6 +215,8 @@ else
 						$sectionListParams["COUNT_ELEMENTS_FILTER"] = "CNT_AVAILABLE";
 					}
 				}
+
+
 				$APPLICATION->IncludeComponent(
 					"bitrix:catalog.section.list",
 					"",
