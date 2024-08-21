6e8f2a37676f14b0826be3b40fbdf9cc76b1e29f
poluektovaalina <poluektova2315@gmail.com>
2024-08-21 11:59:54 +0500

    refactor: отладочные работы по страницам: Наши кейсы : готовы.

--- public_html/local/components/exsolcom/custom.elements/templates/cases.section/template.php
+++ public_html/local/components/exsolcom/custom.elements/templates/cases.section/template.php
@@ -5,7 +5,7 @@ if($arResult):?>
 	<div class="i_projects-section">
 		<?foreach ($arResult['ELEMENT'] as $element){?>
 			<div class="i_projects-section__item">
-				<a href="<?=$element['SECTION_PAGE_URL']?>" >
+				<a href="<?=$element['SECTION_PAGE_URL']?>" class="i_projects-item">
 					<div class="i_projects-section__item__img">
 						<img src="<?=\CFile::GetPath($element['PICTURE'])?>" alt="<?=$element['NAME']?>">
 					</div>
