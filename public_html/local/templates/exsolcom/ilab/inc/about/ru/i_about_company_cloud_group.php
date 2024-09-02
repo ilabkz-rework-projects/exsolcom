308d574bb76706db56593594c6ecb4858fa3d233
poluektovaalina <poluektova2315@gmail.com>
2024-09-02 18:30:26 +0500

    refactor: страница О Компании, языковые версии готово 85%.

---
+++ public_html/local/templates/exsolcom/ilab/inc/about/ru/i_about_company_cloud_group.php
@@ -0,0 +1,25 @@
+<div class="i_cloud-group-desc">
+
+    <div class="i_cloud-group-desc-name">
+        <span><span class="i_cloud-desc-name-color">1C:</span>FRESH</span>
+    </div>
+
+        <div class="i_cloud-group-desc-depiction">
+        <span class="i_cloud-depiction-weight">Облачный сервис для работы с программами «1С» из любой точки мира через интернет</span>
+        <span>
+                            Позволяет работать с программами фирмы «1С» без установки на компьютер, из любого места, где есть доступ к интернету. Это привносит уникальную гибкость в управление бизнесом, позволяя работать с данными в режиме реального времени, где бы вы ни находились.
+                        </span>
+        <span>
+                            Кроме того, сервис предлагает различные функции для администрирования баз данных, что облегчает управление информационными системами предприятия.
+                        </span>
+    </div>
+
+        <div class="i_cloud-group-desc-btn">
+        <a href="https://exsolcom-1cfresh.kz/">
+            <button>
+                Подробнее
+            </button>
+        </a>
+    </div>
+
+</div>
