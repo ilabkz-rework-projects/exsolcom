1ef205c98a0ed3c1cdcf9263f4bc3c1ef98dd497
poluektovaalina <poluektova2315@gmail.com>
2024-09-04 19:17:12 +0500

    refactor: переввод страниц :  контакты

---
+++ public_html/local/templates/exsolcom/ilab/inc/ru/i_email.php
@@ -0,0 +1,5 @@
+<div class="i_contacts-info-item-name">
+    <a href="mailto:info@exsolcom.kz">
+        <span>info@exsolcom.kz</span>
+    </a>
+</div>
---
+++ public_html/local/templates/exsolcom/ilab/inc/ru/i_telephone.php
@@ -0,0 +1,5 @@
+<div class="i_contacts-info-item-name">
+    <a href="tel:+77005010039">
+        <span>+ 7 700 501 00 39</span>
+    </a>
+</div>
