3e667e3ba88fdb2c2da8461d4a1aad4ee8b23bc2
poluektovaalina <poluektova2315@gmail.com>
2024-09-23 13:02:15 +0500

    refactor: поправила переключатель на странице программных продуков

--- public_html/local/templates/exsolcom/components/bitrix/catalog/i_programm/lang/en/section_horizontal.php
+++ public_html/local/templates/exsolcom/components/bitrix/catalog/i_programm/lang/en/section_horizontal.php
@@ -2,6 +2,6 @@
 $MESS["CT_GIFTS_SECTION_LIST_BLOCK_TITLE_DEFAULT"] = "Gifts for products in this catalog";
 $MESS["I_PRICE_SORT"] = "Sort by price:";
 $MESS["I_PRICE_SORT_ASC"] = "Cheapest first";
-$MESS["I_PRICE_SORT_DESC"] = "Most expensive first";
+$MESS["I_PRICE_SORT_DESC"] = "Expensive first";
 $MESS["I_PRICE_SORT_SORT"] = "Compare";
 ?>
