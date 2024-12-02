e490d8a4c87c1cd2b23b5e4c18ee301a21b663e0
Руслан <rd@ilab.kz>
2024-12-02 12:33:51 +0500

    отладка

--- public_html/local/templates/exsolcom/ilab/ajax/getProjectElementModalContent.php
+++ public_html/local/templates/exsolcom/ilab/ajax/getProjectElementModalContent.php
@@ -12,7 +12,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$getData = json_decode($json, true);
 
 	$res = CIBlockElement::GetList([], ['IBLOCK_ID' => $getData['iblockId']], false, false,
-		['PROPERTY_I_CONTENT_LINK', 'PROPERTY_I_CONTENT_LINK_KZ', 'PROPERTY_I_CONTENT_LINK_EN', 'ID', 'NAME', 'PREVIEW_PICTURE', 'DETAIL_PICTURE', 'PREVIEW_TEXT', 'PROPERTY_I_PROGRAMM_LINK']
+		['PROPERTY_I_CONTENT_LINK', 'PROPERTY_I_CONTENT_LINK_KZ', 'PROPERTY_I_CONTENT_LINK_EN', 'CODE', 'ID', 'NAME', 'PREVIEW_PICTURE', 'DETAIL_PICTURE', 'PREVIEW_TEXT', 'PROPERTY_I_PROGRAMM_LINK']
 	);
 	$arResult = [];
 
@@ -32,6 +32,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 			$arResult['NAME'] = $ob['NAME'];
 			$arResult['IMAGE'] = CFile::GetPath($ob['PREVIEW_PICTURE']);
 			$arResult['PREVIEW_TEXT'] = $ob['PREVIEW_TEXT'];
+			$arResult['CODE'] = $ob['CODE'];
 
 			$resProgramm = CIBlockElement::GetList([], ['IBLOCK_ID' => 9, 'ID' => $ob['PROPERTY_I_PROGRAMM_LINK_VALUE']], false, false, ['PROPERTY_I_CONTENT_LINK', 'ID', 'CODE' , 'NAME', 'PREVIEW_PICTURE', 'DETAIL_PICTURE', 'PREVIEW_TEXT', 'PROPERTY_I_PRICE']);
 
