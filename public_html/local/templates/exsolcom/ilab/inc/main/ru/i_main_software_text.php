64386f1b606e5b38c4c7eb7041a4da99b72f22c5
Руслан <rd@ilab.kz>
2025-01-24 16:29:43 +0500

    изменение названия блока i_main_software_text.php в зависимости от того где находимся

--- public_html/local/templates/exsolcom/ilab/inc/main/ru/i_main_software_text.php
+++ public_html/local/templates/exsolcom/ilab/inc/main/ru/i_main_software_text.php
@@ -1,6 +1,18 @@
+<?php
+
+if($_SESSION['CURRENT_SECTION_NAME']){
+	$sectionName = $_SESSION['CURRENT_SECTION_NAME'];
+	unset($_SESSION['CURRENT_SECTION_NAME']);
+}else{
+	$sectionName = 'Линейка корпоративных продуктов для Казахстана';
+}
+
+?>
+
+
 <div class="i_software-group-textBtn">
 	<div class="i_software-group-text">
-		<span class="i_software-headline">Линейка корпоративных продуктов для Казахстана</span>
+		<span class="i_software-headline"><?=$sectionName?></span>
 		<p class="i_software-desc">
 			Предназначена для решения задач управления финансами и ведения учета, как обособленных предприятий, так и предприятий, объединенных в группы (холдинги, конгломераты, группы компаний).
 		</p>
