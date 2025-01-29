5740f367250bb52433fa4685bba5386acfe0fe7e
Руслан <rd@ilab.kz>
2025-01-29 17:17:52 +0500

    Отладка

--- public_html/local/templates/exsolcom/ilab/inc/about/ru/i_about_company-strategy-btn.php
+++ public_html/local/templates/exsolcom/ilab/inc/about/ru/i_about_company-strategy-btn.php
@@ -1,7 +1,5 @@
 <div class="i_about-company-strategy-btn">
-    <a href="../ilab/О_компании_Excolcom_2023_НОВАЯ (1) (9).pdf" target="_blank" download>
-        <button>
-            Скачать презентацию компаний
-        </button>
-    </a>
+ <a href="../ilab/Презентация_O_КОМПАНИИ_ExSolCom_2024_rus.pdf" target="_blank" download=""> <button>
+	Скачать презентацию компаний </button> </a>
 </div>
+ <br>
