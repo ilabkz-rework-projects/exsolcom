9e3c648982569161d37da3ec63aa4a2b4564eeeb
Руслан <rd@ilab.kz>
2025-02-13 11:24:29 +0500

    отладка

--- public_html/local/templates/exsolcom/ilab/inc/about/work_process_blocks/ru/i_work_process_block_1.php
+++ public_html/local/templates/exsolcom/ilab/inc/about/work_process_blocks/ru/i_work_process_block_1.php
@@ -1,4 +1,5 @@
 <div class="i_work-process-body-group-block">
- <span class="i_work-process-body-group-block-color">Анализ, Отчет Обследования</span> <span>
-	Анализ существующих процессов, разработка рекомендаций по оптимизации и подготовка к информационной системе </span>
+ <span class="i_work-process-body-group-block-color">Предпроектное обследование</span> <span>
+	Предпроектное обследование -  отдельный
+проект и контракт – написания технического задания  (ТЗ) на языке бизнес пользователей</span>
 </div>
