f4edb2f0206aa96c4e29808b1d3e1fa93ec47802
poluektovaalina <poluektova2315@gmail.com>
2025-06-04 14:04:23 +0500

    выгрузить создание компоненты и подготовить html для вывода данных. Правки мелкие по сайту по контенту + верстке + отделила стили сайта от битрикс меню админки

---
+++ public_html/local/templates/exsolcom/components/bitrix/search.page/i_search-page/lang/kz/.parameters.php
@@ -0,0 +1,12 @@
+<?
+$MESS["TP_BSP_USE_SUGGEST"] = "Іздеу тіркестері бойынша кеңесті көрсету";
+$MESS["TP_BSP_SHOW_RATING"] = "Рейтингті қосу";
+$MESS["TP_BSP_SHOW_RATING_CONFIG"] = "әдеттегі мәні бойынша";
+$MESS["TP_BSP_RATING_TYPE"] = "Рейтинг түймелерінің түрі";
+$MESS["TP_BSP_RATING_TYPE_CONFIG"] = "әдеттегі мәні бойынша";
+$MESS["TP_BSP_RATING_TYPE_STANDART_TEXT"] = "Ұнайды / Ұнамайды (мәтіндік)";
+$MESS["TP_BSP_RATING_TYPE_STANDART_GRAPHIC"] = "Ұнайды / Ұнамайды (графикалық)";
+$MESS["TP_BSP_RATING_TYPE_LIKE_TEXT"] = "Маған ұнайды (мәтіндік)";
+$MESS["TP_BSP_RATING_TYPE_LIKE_GRAPHIC"] = "Маған ұнайды (графикалық)";
+$MESS["TP_BSP_PATH_TO_USER_PROFILE"] = "Пайдаланушы профиліне апаратын жол үлгісі";
+?>
